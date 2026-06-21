74046c441f1cc83a6e984176d0de8577e10baa7c
Pachicom <mkataev858@gmail.com>
2026-06-21 13:50:42 +0500

    Delete Pachicom_Portfolio directory

--- Pachicom_Portfolio/script.js
+++
@@ -1,3 +0,0 @@
-new Typed('#typed',{strings:['Python Developer','C++ Developer','Java Developer','Game Developer'],typeSpeed:60,backSpeed:35,loop:true});
-ScrollReveal().reveal('.reveal',{distance:'50px',duration:1000,interval:200});
-tsParticles.load('particles',{particles:{number:{value:70},move:{enable:true,speed:1},size:{value:2},color:{value:'#ffffff'}}});
