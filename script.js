51edc4e3d2b54ee6ba593e10d86d44e19b30c64a
Pachicom <mkataev858@gmail.com>
2026-06-21 13:51:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+new Typed('#typed',{strings:['Python Developer','C++ Developer','Java Developer','Game Developer'],typeSpeed:60,backSpeed:35,loop:true});
+ScrollReveal().reveal('.reveal',{distance:'50px',duration:1000,interval:200});
+tsParticles.load('particles',{particles:{number:{value:70},move:{enable:true,speed:1},size:{value:2},color:{value:'#ffffff'}}});
